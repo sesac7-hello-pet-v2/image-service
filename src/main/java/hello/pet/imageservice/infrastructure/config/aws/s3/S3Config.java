2b80436bb5b0e0f86b0a340a150132387648adac
namung08 <namung08@gmail.com>
2025-09-30 16:12:34 +0900

    :fire: chore: 사용하지 않는 코드 삭제
    Signed-off-by: namung08 <namung08@gmail.com>

--- src/main/java/hello/pet/imageservice/infrastructure/config/aws/s3/S3Config.java
+++ src/main/java/hello/pet/imageservice/infrastructure/config/aws/s3/S3Config.java
@@ -11,7 +11,6 @@ import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -27,14 +26,6 @@ public class S3Config {
 	@Bean
 	public S3Client s3Client() {
 		return S3Client.builder()
-			.region(Region.of(region))
-			.credentialsProvider(awsCredentialsProvider())
-			.build();
-	}
-
-	@Bean
-	public S3Presigner s3Presigner() {
-		return S3Presigner.builder()
 			.region(Region.of(region))
 			.credentialsProvider(awsCredentialsProvider())
 			.build();
